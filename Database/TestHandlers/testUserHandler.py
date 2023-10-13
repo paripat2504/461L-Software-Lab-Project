363469f3d62a450cf54b3aa6e88196af4bbf79a4
Emmanuel Ahonle <emmanuelahonle101@utexas.edu>
2023-10-12 20:21:29 -0500

    rearranging database directory structure

---
+++ Database/TestHandlers/testUserHandler.py
@@ -0,0 +1,35 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+from Handlers import UserHandler
+import Database_Constants
+import unittest
+import pymongo
+
+# Replace these with your MongoDB connection details
+
+mongo = Database_Constants.InitializeGlobals()
+mongo_url = mongo.getMongoURL()  # Default MongoDB connection URL
+users = mongo.getUsers()
+
+
+
+class testUserHandler(unittest.TestCase):
+    def testAddAndDropUser(self):
+        UserHandler.addUser("newUser2", "userLogin", "passwd", ["Numpy Ninjas", "Team"])
+        returnVal = UserHandler.findUser({'userID' : "userLogin"}, {'userLogin' : 1 , '_id' : 0})
+        self.assertEqual({'userID' : 'userLogin'},returnVal)
+        UserHandler.dropUser('userLogin')
+
+    # def testEditUser(self):
+    #     UserHandler.addUser("newUser2", "userLogin", "passwd", ["Numpy Ninjas", "Team"])
+    #     UserHandler.dropUser('userLogin')
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+
