c491ad569d3a3b0194c515680bd08acb8b78da09
Emmanuel Ahonle <emmanuelahonle101@utexas.edu>
2023-10-12 21:22:17 -0500

    changes

--- softwareApp/databasescript.py
+++ softwareApp/databasescript.py
@@ -1,7 +1,8 @@
-import Database.Handlers.UserHandler
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Database.Handlers import UserHandler
 class Database:
 
     def __init__(self):
