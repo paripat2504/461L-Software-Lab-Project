b68317b124821ecbf86da0de2afe86e43e5c3c97
skvarre <skvaertill@hotmail.com>
2023-11-01 22:09:46 -0500

    add styling for login page

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -45,10 +45,10 @@ function LoginForm() {
       <div className="bg-blue-70 flex justify-center items-center h-screen w-screen">
       <div className=" border-t-8 rounded-sm border-indigo-600 bg-white p-12 shadow-2xl w-96">
       <form onSubmit={handleClick}>
-        <h1 className="font-italic text-center block text-2xl">Login Page</h1>
+        <h1 className="font-italic text-center block text-2xl">Log In</h1>
           <div>
             <label className="text-gray-500 block mt-3">User ID</label>
-            <input type="text"
+            <input className="rounded px-4 py-3 w-full mt-1 bg-white text-gray-900 border border-gray-200 focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-100" type="text"
               placeholder='Enter your User ID'
               style={{marginRight:'10px'}}
               value={username}
@@ -56,16 +56,17 @@ function LoginForm() {
             />
           </div>
           <div>
-            <label>Password</label>
-            <input type="password"
+            <label className="text-gray-500 block mt-3">Password</label>
+            <input className="rounded px-4 py-3 w-full mt-1 bg-white text-gray-900 border border-gray-200 focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-100" type="password"
               placeholder='Enter your password'
               value={password}
               onChange={handlePassword}  
             />
           </div>
-          <button type="submit" className="mt-6 transition transition-all block py-3 px-4 w-full text-white font-bold rounded cursor-pointer bg-gradient-to-r from-indigo-600 to-purple-400 hover:from-indigo-700 hover:to-purple-500 focus:bg-indigo-900 transform hover:-translate-y-1 hover:shadow-lg" onClick={handleClick} style={{marginRight:'20px'}}>Login</button>
+          <div className="text-red-600 flex justify-center">{message}</div>
+          <button type="submit" className="mt-6 transition transition-all block py-3 px-4 w-full text-white font-bold rounded cursor-pointer bg-gradient-to-r from-indigo-600 to-purple-400 hover:from-indigo-700 hover:to-purple-500 focus:bg-indigo-900 transform hover:-translate-y-1 hover:shadow-lg" onClick={handleClick} style={{marginRight:'20px'}}>Submit</button>
           <button type="button" className="mt-6 transition transition-all block py-3 px-4 w-full text-white font-bold rounded cursor-pointer bg-gradient-to-r from-indigo-600 to-purple-400 hover:from-indigo-700 hover:to-purple-500 focus:bg-indigo-900 transform hover:-translate-y-1 hover:shadow-lg"onClick={handleSignupClick}>Sign up</button>
-          <div>{message}</div>
+          
     </form>
     </div>
     </div>
